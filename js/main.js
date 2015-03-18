6f8fcc30f49230e80fb33b8d431333db8ef2910e
Brendan  Wilson <brendan307@Macintosh-7.local>
2015-03-18 00:44:37 -0400

    lightbox, effects, transitions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 
 var navtop = document.querySelector('.nav-top');
+var $thumbs = $('.thumbnails');
+var $lbImg = $('.lightbox-img');
+var $lb = $('.lightbox');
+var $btnClose = $('.btn-close');
 
 document.querySelector('.nav-btn').addEventListener('click', function (e) {
     e.preventDefault();
@@ -10,4 +14,15 @@ document.querySelector('.nav-btn').addEventListener('click', function (e) {
     navtop.setAttribute('data-state', 'expanded');
     this.setAttribute('data-state', 'active');
   }
+});
+
+$thumbs.on('click', 'a', function (e) {
+    e.preventDefault();
+    var big = $(this).attr('href');
+    $lbImg.attr('src', big);
+    $lb.attr('data-state', 'visible');
+});
+
+$btnClose.on('click', function () {
+    $lb.attr('data-state', 'hidden');
 });
